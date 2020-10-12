fa64c9276b53111b70ee26aa80c515a3fdfedc4a
MilleKel <MilleKel@TS31528.NIACC.EDU>
2020-10-12 18:55:00 -0500

    Initial commit

---
+++ public_html/LoopsPractice2.js
@@ -0,0 +1,8 @@
+/* 
+ * Ask the user to enter 5 numbers.
+ * Keep a running total.
+ * Display the final total.
+ */
+
+document.write("This program asks the user for 5 numbers and prints their total.");
+
