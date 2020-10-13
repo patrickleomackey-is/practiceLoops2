139f4bdb05170e46a24478c7b9c973e06cd1bf8f
millekel <millekel@T29148.niaccit.niacc.edu>
2020-10-13 09:57:55 -0500

    Sum of user numbers

--- public_html/LoopsPractice2.js
+++ public_html/LoopsPractice2.js
@@ -5,4 +5,13 @@
  */
 
 document.write("This program asks the user for 5 numbers and prints their total.");
+let counter = 1;
+let sum = 0;
+let userNumber = 0;
+while (counter <= 5) {
+    userNumber = Number(prompt("Enter number " + counter + ": "));
+    sum = sum + userNumber;
+    counter = counter + 1;
+}
+document.write("<br>The sum is " + sum);
 
